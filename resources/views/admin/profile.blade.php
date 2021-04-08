4b119a453c89a92bc9cfcb83b09a4994c17d10ee
alifirhas <irhasalif@gmail.com>
2021-04-08 13:05:43 +0700

    seeder

--- resources/views/admin/profile.blade.php
+++ resources/views/admin/profile.blade.php
@@ -19,7 +19,7 @@ Profile | Tailwind Starter Kit by Creative Tim
 @section('main')
 <section class="relative block" style="height: 500px;">
   <div class="absolute top-0 w-full h-full bg-center bg-cover"
-    style='background-image: url("https://images.unsplash.com/photo-1499336315816-097655dcfbda?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=2710&amp;q=80");'>
+    style='background-image: url("{{ asset('img/photo-1.jpeg') }}");'>
     <span id="blackOverlay" class="w-full h-full absolute opacity-50 bg-black"></span>
   </div>
   <div class="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden"
